0759242c2b9924cf5aa4cbb9bb21f6875d22e83b
Rafael Alfaro <111470786+rrAlfaro@users.noreply.github.com>
2025-11-24 19:05:26 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bianca, te amo, Se você me ama, dá uma risadinha! :D </h1>
+  return <h1>Bianca, te amo, Se você me ama, dá uma risadinha! :D </h1>;
 }
 
 export default Home;
