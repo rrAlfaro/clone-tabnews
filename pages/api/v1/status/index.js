2e031ad2c8fc0da88180c8dcd0db19336d290cab
Rafael Alfaro <111470786+rrAlfaro@users.noreply.github.com>
2025-11-30 16:09:50 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sun;");
+  console.log(result.rows);
   response
     .status(200)
     .json({ chave: "alunos do curso.dev são pessoas acima da média" });
 }
+
+export default status;
