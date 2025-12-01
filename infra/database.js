a9565206acda8e65fdf453ae5ed9cbda100ade8e
Rafael Alfaro <111470786+rrAlfaro@users.noreply.github.com>
2025-12-01 13:41:18 +0000

    add a more robust ssl configuration

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
   console.log("Crendenciais do Postgres", {
     host: process.env.POSTGRES_HOST,
@@ -31,3 +31,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
